3aafdcfc4827ec00dc4f597b2333a4e2a207df31
baoqi <baoqi@didiglobal.com>
2021-09-26 19:21:40 +0800

    fix:bug

--- model/service_dto.go
+++ model/service_dto.go
@@ -30,11 +30,11 @@ func (param *ServiceAddInput) BindValidParam(c *gin.Context) error {
 }
 
 type ServiceAddInput struct {
-	ServiceName         string `json:"service_name" form:"service_name" comment:"服务名称" example:"" validate:"required,valid_service_name"` //服务名称
-	ServiceDesc         string `json:"service_desc" form:"service_desc" comment:"服务描述" example:"" validate:"required,valid_service_desc"` //服务描述
+	ServiceName         string `json:"service_name" form:"service_name" comment:"服务名称" example:"" validate:"required"` //服务名称
+	ServiceDesc         string `json:"service_desc" form:"service_desc" comment:"服务描述" example:"" validate:"required"` //服务描述
 	LoadType            int    `json:"load_type" form:"load_type" comment:"负载类型 0=http 1=tcp 2=grpc"`
-	HTTPHosts           string `json:"http_hosts" form:"http_hosts" comment:"域名信息" example:"" validate:"required,valid_http_hosts"`
-	HTTPPaths           string `json:"http_paths" form:"http_paths" comment:"路径信息" example:"" validate:"required,valid_http_paths"`
+	HTTPHosts           string `json:"http_hosts" form:"http_hosts" comment:"域名信息" example:"" validate:"required"`
+	HTTPPaths           string `json:"http_paths" form:"http_paths" comment:"路径信息" example:"" validate:"required"`
 	NeedStripUri        string `json:"need_strip_uri" form:"need_strip_uri" comment:"启用strip_uri" example:"" validate:"max=1,min=0"` //启用strip_uri
 	Port                int    `json:"port" form:"port" comment:"tcp/grpc端口"`
 	LoadBalanceStrategy string `json:"load_balance_strategy" form:"load_balance_strategy" comment:"负载策略"`
